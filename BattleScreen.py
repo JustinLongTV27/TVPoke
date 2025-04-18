23628bd1f2581913a220d98bcde5f0124bcf6987
JustinLongTV27 <long.justintaylor@gmail.com>
2025-04-18 11:11:20 -0700

    yooo

--- BattleScreen.py
+++ BattleScreen.py
@@ -11,32 +11,42 @@ class BattleScreen(Screen):
             Trainer(trainer1Poke),
             Trainer(trainer2Poke)
         ]
-        self.aciveTrainer = self.trainers[0]
+        self.activeTrainer = self.trainers[0]
         
     def elementsToDisplay(self):
         self.elements = []
 
         for trainer in self.trainers:
+            if trainer == self.activeTrainer:
+                trainer.position = 1
+            else:
+                trainer.position = 2
+
+        for trainer in self.trainers:
             self.elements.extend(trainer.getPageElements())
 
         for trainer in self.trainers:
-            if self.aciveTrainer == trainer:
+            if self.activeTrainer == trainer:
                 trainer.postion = 1
             else:
                 trainer.position = 2
 
         if self.activeTrainer == self.trainers[0]:
-            self.activetrianer = self.trainers[1]
+            self.activeTrianer = self.trainers[1]
         else:
             self.activeTrainer = self.trainers[0]
 
 
 
 class BattleButtons(Button):
-    def __init__(self, position, width, height, text):
+    def __init__(self, position, width, height, text, imgPath):
         super().__init__(position, width, height, text)
+        self.imgPath = imgPath
 
 
+class BackRound(Image):
+    def __init__(self, position, width, height, img):
+        super().__init__(position, width, height, img)
 
         #y = 0
         ##two rows of three
--- TVPoke/BaseClasses/Trainer.py
+++ TVPoke/BaseClasses/Trainer.py
@@ -4,6 +4,7 @@ from PyUI.PageElements import Label, Image
 class Trainer:
     def __init__(self, pokemon):
         self.pokemon = []
+        self.position = 1
         for poke in pokemon:
             pokeFile = importlib.import_module("TVPoke.Pokemon." + poke)
             PokeClass = getattr(pokeFile, poke)
@@ -19,13 +20,13 @@ class Trainer:
 
         pokeHealth = self.pokemon[0].hp
         pokeName = self.pokemon[0].name
-        pokeImg = self.pokemon[0].imgPath
+        pokeImg = self.pokemon[0].img
 
         if self.position == 1:
             pokeX = 25
             pokeY = 25
             
-            healthLabel = Label((pokeX, pokeY), 20, 10, pokeName + "Your HP: " + pokeHealth)
+            healthLabel = Label((pokeX, pokeY + 10), 20, 10, pokeName + "Your HP: " + str(pokeHealth))
             thePokemon = Image((pokeX, pokeY), 20, 20, pokeImg)
             elements.append(healthLabel)
             elements.append(thePokemon)
@@ -33,8 +34,9 @@ class Trainer:
             pokeX = 75
             pokeY = 75
 
-            healthLabel = Label((pokeX, pokeY), 20, 10, pokeName + "Enemy HP: " + pokeHealth)
+            healthLabel = Label((pokeX, pokeY + 10), 20, 10, pokeName + "Enemy HP: " + str(pokeHealth))
             thePokemon = Image((pokeX, pokeY), 20, 20, pokeImg)
         
         elements.append(healthLabel)
         elements.append(thePokemon)
+        return elements
